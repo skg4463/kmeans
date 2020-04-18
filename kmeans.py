0c82d411d7561a748ff6d008ea37d387896bde98
skg4463 <skg4463>
2020-04-19 08:59:58 +0900

    edit

--- kmeans.py
+++ kmeans.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-image = cv2.imread("Lena.jpg")
+image = cv2.imread("fruit_color.jpg")
 
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 pixel_values = image.reshape((-1, 3))
@@ -11,7 +11,7 @@ pixel_values = np.float32(pixel_values)
 
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
 
-K = 2
+K = 4
 
 print("pixels :", pixel_values.shape[0])
 print("Cluster: ", K)
@@ -28,6 +28,7 @@ for i in range(K):
     print("-------\nClass", i)
     print(round((labels == i).sum()/pixel_values.shape[0], 4)*100, "%")
 
+print(pixel_values[0])
 
 segmented_image = segmented_image.reshape(image.shape)
 plt.imshow(segmented_image)
